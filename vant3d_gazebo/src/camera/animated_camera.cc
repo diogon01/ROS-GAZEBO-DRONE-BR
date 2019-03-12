4db277c380e64fa6d367c65772e9fe7c1a5acc33
dregner <daniel_regner@hotmail.com>
2019-03-12 12:01:19 -0300

    Remoção de sombra e adiçao de textura da superficie do mar

--- vant3d_gazebo/src/camera/animated_camera.cc
+++ vant3d_gazebo/src/camera/animated_camera.cc
@@ -32,7 +32,7 @@ namespace gazebo {
                     // name the animation "test",
                     // make it last 10 seconds,
                     // and set it on a repeat loop
-                    new gazebo::common::PoseAnimation("test", 10.0, true));
+                    new gazebo::common::PoseAnimation("test", 30.0, true));
 
             gazebo::common::PoseKeyFrame *key;
 
@@ -42,29 +42,29 @@ namespace gazebo {
             key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
             // set waypoint location after 2 seconds
-            key = anim->CreateKeyFrame(2.0);
-            key->Translation(ignition::math::Vector3d(-50, -50, 0));
-            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
-
-
-            key = anim->CreateKeyFrame(4.0);
-            key->Translation(ignition::math::Vector3d(10, 20, 0));
-            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
-
-
-            key = anim->CreateKeyFrame(6.0);
-            key->Translation(ignition::math::Vector3d(-10, 20, 0));
-            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
-
-
-            key = anim->CreateKeyFrame(8.0);
-            key->Translation(ignition::math::Vector3d(10, -20, 0));
-            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
-
-            // set final location equal to starting location
-            key = anim->CreateKeyFrame(10);
-            key->Translation(ignition::math::Vector3d(0, 0, 0));
+            key = anim->CreateKeyFrame(15.0);
+            key->Translation(ignition::math::Vector3d(0, 0, 15));
             key->Rotation(ignition::math::Quaterniond(0, 0, 0));
+
+//
+//            key = anim->CreateKeyFrame(4.0);
+//            key->Translation(ignition::math::Vector3d(10, 20, 0));
+//            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
+//
+//
+//            key = anim->CreateKeyFrame(6.0);
+//            key->Translation(ignition::math::Vector3d(-10, 20, 0));
+//            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
+//
+//
+//            key = anim->CreateKeyFrame(8.0);
+//            key->Translation(ignition::math::Vector3d(10, -20, 0));
+//            key->Rotation(ignition::math::Quaterniond(0, 0, 1.5707));
+//
+//            // set final location equal to starting location
+//            key = anim->CreateKeyFrame(10);
+//            key->Translation(ignition::math::Vector3d(0, 0, 0));
+//            key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
             // set the animation
             _parent->SetAnimation(anim);
