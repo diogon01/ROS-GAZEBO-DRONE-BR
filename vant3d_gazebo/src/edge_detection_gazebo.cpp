2fdd1cdfd3a9d96330dc920947b27a207203d065
Diogo Henrique <diogo.henrique.df@gmail.com>
2019-03-27 16:36:47 -0300

    Primeira funcionalidade salvar foi montada! Depois vamos refinar o processo.

--- vant3d_gazebo/src/edge_detection_gazebo.cpp
+++ vant3d_gazebo/src/edge_detection_gazebo.cpp
@@ -8,87 +8,90 @@
 static const std::string OPENCV_WINDOW = "Raw Image window";
 static const std::string OPENCV_WINDOW_1 = "Edge Detection";
 
-class Edge_Detector
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  
+class Edge_Detector {
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+
 public:
-  Edge_Detector()
-    : it_(nh_)
-  {
-    // Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/ilyess/camera1/image_raw", 1, 
-      &Edge_Detector::imageCb, this);
-    image_pub_ = it_.advertise("/edge_detector/raw_image", 1);
-    cv::namedWindow(OPENCV_WINDOW);
+    Edge_Detector()
+            : it_(nh_) {
+        // Subscribe to input video feed and publish output video feed
+        image_sub_ = it_.subscribe("/ilyess/camera1/image_raw", 1,
+                                   &Edge_Detector::imageCb, this);
+        image_pub_ = it_.advertise("/edge_detector/raw_image", 1);
+        cv::namedWindow(OPENCV_WINDOW);
 
-  }
-
-  ~Edge_Detector()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-
-    cv_bridge::CvImagePtr cv_ptr;
-    namespace enc = sensor_msgs::image_encodings;
-
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
     }
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 400 && cv_ptr->image.cols > 600){
+    ~Edge_Detector() {
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
 
-  detect_edges(cv_ptr->image);
-      image_pub_.publish(cv_ptr->toImageMsg());
+    void imageCb(const sensor_msgs::ImageConstPtr &msg) {
 
-  }
-  }
-  void detect_edges(cv::Mat img)
-  {
+        cv_bridge::CvImagePtr cv_ptr;
+        namespace enc = sensor_msgs::image_encodings;
 
-    cv::Mat src, src_gray;
-  cv::Mat dst, detected_edges;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception &e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
 
-  int edgeThresh = 1;
-  int lowThreshold = 200;
-  int highThreshold =300;
-  int kernel_size = 5;
+        // Draw an example circle on the video stream
+        if (cv_ptr->image.rows > 400 && cv_ptr->image.cols > 600) {
 
-  img.copyTo(src);
+            detect_edges(cv_ptr->image);
+            image_pub_.publish(cv_ptr->toImageMsg());
 
-  cv::cvtColor( img, src_gray, CV_BGR2GRAY );
-        cv::blur( src_gray, detected_edges, cv::Size(5,5) );
-  cv::Canny( detected_edges, detected_edges, lowThreshold, highThreshold, kernel_size );
+            // TODO: Alocar o metdo em um local correto
+            static int image_count = 0;
+            std::stringstream sstream;
+            sstream << "labmetro_image" << image_count << ".png";
+            ROS_ASSERT(cv::imwrite(sstream.str(), cv_ptr->image));
+            image_count++;
 
-    dst = cv::Scalar::all(0);
-    img.copyTo( dst, detected_edges);
-  dst.copyTo(img);
+            std::cerr << "\n Imagem salva com sucesso:  [" <<
+                      sstream.str() << "]\n";
 
-      cv::imshow(OPENCV_WINDOW, src);
-      cv::imshow(OPENCV_WINDOW_1, dst);
-      cv::waitKey(3);
+        }
+    }
 
-  } 
- 
+    void detect_edges(cv::Mat img) {
+
+        cv::Mat src, src_gray;
+        cv::Mat dst, detected_edges;
+
+        int edgeThresh = 1;
+        int lowThreshold = 200;
+        int highThreshold = 300;
+        int kernel_size = 5;
+
+        img.copyTo(src);
+
+        cv::cvtColor(img, src_gray, CV_BGR2GRAY);
+        cv::blur(src_gray, detected_edges, cv::Size(5, 5));
+        cv::Canny(detected_edges, detected_edges, lowThreshold, highThreshold, kernel_size);
+
+        dst = cv::Scalar::all(0);
+        img.copyTo(dst, detected_edges);
+        dst.copyTo(img);
+
+        cv::imshow(OPENCV_WINDOW, src);
+        cv::imshow(OPENCV_WINDOW_1, dst);
+        cv::waitKey(3);
+
+    }
+
 };
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "Edge_Detector");
-  Edge_Detector ic;
-  ros::spin();
-  return 0;
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "Edge_Detector");
+    Edge_Detector ic;
+    ros::spin();
+    return 0;
 }
